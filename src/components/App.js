d98aeabb35c31404d7dea0c8a45d4047abb9691d
MohammadOwais10 <owais0945@gmail.com>
2022-02-02 21:16:22 +0530

    useAuth in Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,15 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { getPosts } from '../api';
 import { useAuth } from '../hooks';
 import { Home, Login } from '../pages';
 import { Loader, Navbar } from './';
-
-const About = () => {
-  return <h1>About</h1>;
-};
-
-const UserInfo = () => {
-  return <h1>User</h1>;
-};
 
 const Page404 = () => {
   return <h1>404</h1>;
 };
 
 function App() {
-  // const [posts, setPosts] = useState([]);
-  // const [loading, setLoading] = useState(true);
   const auth = useAuth();
-
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     const response = await getPosts();
-
-  //     if (response.success) {
-  //       setPosts(response.data.posts);
-  //     }
-
-  //     setLoading(false);
-  //   };
-
-  //   fetchPosts();
-  // }, []);
 
   if (auth.loading) {
     return <Loader />;
@@ -46,13 +20,9 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home posts={[]} />} />
+          <Route exact path="/" element={<Home />} />
 
           <Route exact path="/login" element={<Login />} />
-
-          <Route exact path="/about" element={<About />} />
-
-          <Route exact path="/user/asdasd" element={<UserInfo />} />
 
           <Route element={<Page404 />} />
         </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom';
 import styles from '../styles/navbar.module.css';
+import { useAuth } from '../hooks';
 
 const Navbar = () => {
+  const auth = useAuth();
   return (
     <div className={styles.nav}>
       <div className={styles.leftDiv}>
@@ -14,28 +16,35 @@ const Navbar = () => {
       </div>
 
       <div className={styles.rightNav}>
-        <div className={styles.user}>
-          <a href="/">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/4825/4825123.png"
-              alt=""
-              className={styles.userDp}
-            />
-          </a>
-          <span>Owais</span>
-        </div>
+        {auth.user && (
+          <div className={styles.user}>
+            <a href="/">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/4825/4825123.png"
+                alt=""
+                className={styles.userDp}
+              />
+            </a>
+            <span>{auth.user.name}</span>
+          </div>
+        )}
 
         <div className={styles.navLinks}>
           <ul>
-            <li>
-              <Link to="/login">Log in</Link>
-            </li>
-            <li>
-              <a href="/">Log out</a>
-            </li>
-            <li>
-              <a href="/">Register</a>
-            </li>
+            {auth.user ? (
+              <>
+                <li onClick={auth.logout}>Log out</li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/login">Log in</Link>
+                </li>
+                <li>
+                  <a href="/">Register</a>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
